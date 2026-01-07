dd151eba53c71b8c2902845222a093ad61795074
Ke Han <kehan5800@gmail.com>
2026-01-06 21:51:56 -0500

    update

--- artifact/overhead/cassandra/cass_overhead.sh
+++ artifact/overhead/cassandra/cass_overhead.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# assert: current dir is ~/project/upfuzz
-if [ "$PWD" != "$UPFUZZ_DIR" ]; then
-  echo "Current directory is not ~/project/upfuzz"
-  exit 1
-fi
-
 bash artifact/overhead/cassandra/cass_2.sh true # with DF
 bash artifact/overhead/cassandra/cass_2.sh false # without DF
 
