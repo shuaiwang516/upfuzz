6afac461034317c11ad9cf391a026571cd74e51c
Ke Han <kehan5800@gmail.com>
2026-01-06 21:44:52 -0500

    update

--- artifact/overhead/cassandra/compute.sh
+++ artifact/overhead/cassandra/compute.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# assert that client_normal.log and client_format.log exist
+if [ ! -f "client_normal.log" ]; then
+  echo "client_normal.log does not exist"
+  exit 1
+fi
+if [ ! -f "client_format.log" ]; then
+  echo "client_format.log does not exist"
+  exit 1
+fi
+
 t1_normal=$(grep "Connect to cqlsh" client_normal.log | head -n 1 | awk '{print $2}')
 t2_normal=$(grep "Cqlsh connected" client_normal.log | head -n 1 | awk '{print $2}')
 t3_normal=$(grep "collect coverage" client_normal.log | head -n 1 | awk '{print $2}')
@@ -35,6 +45,9 @@ interval_2_3_ms_format=$((ms3_format - ms2_format))
 overhead1=$(awk "BEGIN {printf \"%.4f\", ($interval_1_2_ms_format - $interval_1_2_ms_normal) / $interval_1_2_ms_normal}")
 overhead2=$(awk "BEGIN {printf \"%.4f\", ($interval_2_3_ms_format - $interval_2_3_ms_normal) / $interval_2_3_ms_normal}")
 
-echo "Overhead 1: $overhead1"
-echo "Overhead 2: $overhead2"
+overhead1_pct=$(awk "BEGIN {printf \"%.2f\", $overhead1 * 100}")
+overhead2_pct=$(awk "BEGIN {printf \"%.2f\", $overhead2 * 100}")
+
+echo "Overhead 1: ${overhead1_pct}%"
+echo "Overhead 2: ${overhead2_pct}%"
 
