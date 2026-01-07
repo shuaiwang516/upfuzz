d61a0eda2b1ef1da8c5cfc9531526585c10b009e
Ke Han <kehan5800@gmail.com>
2026-01-06 21:42:46 -0500

    update

--- artifact/overhead/cassandra/compute.sh
+++ artifact/overhead/cassandra/compute.sh
@@ -11,6 +11,8 @@ t3_format=$(grep "collect coverage" client_format.log | head -n 1 | awk '{print 
 to_ms () {
   local t=$1
   IFS=':,' read -r h m s ms <<< "$t"
+  # Remove leading zeros to avoid octal interpretation
+  h=$((10#$h)); m=$((10#$m)); s=$((10#$s)); ms=$((10#$ms))
   echo $(( (h*3600 + m*60 + s)*1000 + ms ))
 }
 
@@ -30,9 +32,9 @@ interval_2_3_ms_format=$((ms3_format - ms2_format))
 
 # compute overhead: (interval_1_2_ms_format-interval_1_2_ms_normal) / interval_1_2_ms_normal
 
-overhead1 = $((interval_1_2_ms_format - interval_1_2_ms_normal) / interval_1_2_ms_normal)
-overhead2 = $((interval_2_3_ms_format - interval_2_3_ms_normal) / interval_2_3_ms_normal)
+overhead1=$(awk "BEGIN {printf \"%.4f\", ($interval_1_2_ms_format - $interval_1_2_ms_normal) / $interval_1_2_ms_normal}")
+overhead2=$(awk "BEGIN {printf \"%.4f\", ($interval_2_3_ms_format - $interval_2_3_ms_normal) / $interval_2_3_ms_normal}")
 
-echo "Overhead 1: $((interval_1_2_ms_format - interval_1_2_ms_normal) / interval_1_2_ms_normal)"
-echo "Overhead 2: $((interval_2_3_ms_format - interval_2_3_ms_normal) / interval_2_3_ms_normal)"
+echo "Overhead 1: $overhead1"
+echo "Overhead 2: $overhead2"
 
